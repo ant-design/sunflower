d663d9a18d5396598f22517ef84ae837f1ede823
JIACHENG9 <zinkey@vip.qq.com>
2019-06-22 16:03:25 +0800

    docs: fix examples

--- docs/sunflower-search-result/request.js
+++ docs/sunflower-search-result/request.js
@@ -3,22 +3,27 @@ import Mock from 'mockjs';
 
 const total = 200;
 
-const result = Mock.mock({
+const db = Mock.mock({
   [`list|${total}`]: [{
-    name: '@name',
+    username: '@name',
     email: '@email',
     id: '@guid',
   }],
 });
 
-export default ({ pageSize, currentPage }) => {
+export default ({ username, pageSize, currentPage }) => {
   const start = pageSize * (currentPage - 1) + 1;
   const end = start + pageSize;
-  const list = result.list.slice(start, end);
+  let totalList = db.list;
+  if (username) {
+    totalList = totalList.filter(item => (item.username.indexOf(username) > -1));
+  }
+  debugger
+  const list = totalList.slice(start, end);
   return new Promise(r => setTimeout(() => {
     r({
       list,
-      total,
+      total: totalList.length,
     });
   }, 300));
 };
