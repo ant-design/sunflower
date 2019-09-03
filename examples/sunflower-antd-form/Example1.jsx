c478a41e661b586deb436e7484812ca750974bc7
Mr_zyy <1016462699@qq.com>
2019-09-03 17:33:21 +0800

    feat: useForm

---
+++ examples/sunflower-antd-form/Example1.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useForm } from 'sunflower-antd';
+import { Input, Form, message, Button, Spin } from 'antd';
+
+
+export default Form.create()(props => {
+  const { form } = props;
+  const { formProps, loading } = useForm({
+    form,
+    defaultFormValues: { username: 'Lily', email: '10164626@qq.com' },
+    async submit(values) {
+      await new Promise(r => setTimeout(r, 1000));
+      console.log(1);
+      message.success(`${JSON.stringify(values)}提交成功`);
+    },
+  })
+  return (
+    <div>
+      <Spin spinning={loading}>
+        <Form layout="inline" {...formProps}>
+          <Form.Item label="Username">
+            {
+              form.getFieldDecorator('username', {
+                rules: [{ required: true, message: '该字段不能为空' }],
+              })(
+                <Input placeholder="Username" />
+              )
+            } 
+          </Form.Item>
+
+          <Form.Item label="Email">
+            {
+              form.getFieldDecorator('email', {
+                rules: [{ required: true, message: '该字段不能为空' }],
+              })(
+                <Input placeholder="Email" />
+              )
+            } 
+          </Form.Item>
+          <Form.Item>
+            <Button onClick={() => form.resetFields()}>
+              Reset
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Search
+            </Button>
+          </Form.Item>
+        </Form>
+      </Spin>
+    </div>
+  )
+});
---
+++ packages/sunflower-antd-form/src/index.tsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+import { Form } from 'antd';
+
+declare type StoreBaseValue = string | number | boolean;
+export declare type StoreValue = StoreBaseValue | Store | StoreBaseValue[];
+export interface Store {
+  [name: string]: StoreValue;
+}
+
+export interface UseFormConfig<T> {
+  submit?: (formValues: T) => any;
+  defaultFormValues?: Store;
+  form: any;
+}
+
+export const useForm = (config: UseFormConfig<Store>) => {
+  const formConfig = config || {} as UseFormConfig<Store>;
+  const {
+    submit,
+    defaultFormValues = {},
+    form,
+  } = formConfig;
+
+  const [initialValues, setInitialValues] = useState();
+  const [loading, setLoading] = useState(false);
+
+  let version = 3;
+  // antd4
+  if (Form['useForm']) {
+    version = 4;
+  }
+
+  let formInstance = form;
+  if (!form) {
+    if (version === 4) {
+      [formInstance] = Form['useForm']();
+    } else {
+      throw new Error('"form" need in antd@3');
+    }
+  }
+
+  useEffect(() => {
+    if (version === 4) {
+      setInitialValues(defaultFormValues);
+    } else {
+      form.setFieldsValue(defaultFormValues);
+    }
+  }, [])
+
+
+  const onFinish = (values: Store) => {
+    submit({ ...values });
+  };
+
+  const formProps = version === 4 ? {
+    form: formInstance,
+    onFinish,
+    initialValues,
+  } : {
+    onSubmit(e) {
+      e.preventDefault();
+      formInstance.validateFields(async (err, values) => {
+        if (!err) {
+          setLoading(true);
+          await submit({ ...values });
+          setLoading(false);
+        }
+      });
+    },
+  };
+
+  return {
+    formProps,
+    loading,
+  };
+};
