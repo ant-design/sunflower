136dbecff1f6d7d7792c5b422cca62edcb2af4e8
aolin.xal <aolin.xal@alipay.com>
2019-09-18 11:50:04 +0800

    feat: useStepsForm 主代码

---
+++ packages/sunflower-antd-steps-form/src/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useForm, UseFormConfig } from '@sunflower-antd/form'
+
+export interface UseStepsFormConfig extends UseFormConfig {
+  total?: number;
+  defaultCurrent?: number;
+}
+
+export const useStepsForm = (config: UseStepsFormConfig) => {
+  const {
+    form,
+    defaultFormValues,
+    total,
+    defaultCurrent,
+    submit,
+  } = config || {} as UseStepsFormConfig
+  const [current, setCurrent] = useState(defaultCurrent)
+
+  const {
+    form: formInstance,
+    formProps,
+    formLoading,
+    defaultFormValuesLoading,
+    formValues,
+    initialValues,
+    formResult,
+    submit: formSubmit,
+  } = useForm({
+    form,
+    submit,
+    defaultFormValues,
+  })
+
+  const gotoStep = step => {
+    step = Number(step)
+
+    if (step === current) {
+      return
+    }
+
+    // goto the target step after passing validate
+    formInstance.validateFields(err => {
+      if (!err) {
+        setCurrent(step)
+      }
+    })
+  }
+
+  const handleStepChange = currentStep => gotoStep(currentStep)
+
+  return {
+    current,
+    total,
+    gotoStep,
+    stepsProps: {
+      initial: defaultCurrent,
+      current,
+      onChange: handleStepChange,
+    },
+    formProps,
+    formLoading,
+    defaultFormValuesLoading,
+    formValues,
+    initialValues,
+    formResult,
+    form: formInstance,
+    submit: formSubmit,
+  }
+}
