860f61342e7261226cc1b952f0de86e265d52367
JIACHENG9 <zinkey@vip.qq.com>
2020-03-19 22:54:50 +0800

    feat: useForm

---
+++ tests/useForm.test.js
@@ -0,0 +1,33 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useForm } from '../src/useForm';
+
+test('useForm', async () => {
+  const submit = jest.fn();
+  const mockForm = {};
+  const config = {
+    form: mockForm,
+    submit: values => {
+      submit({
+        ...values,
+      });
+      return {
+        list: [
+          {
+            name: 'lily',
+          },
+          {
+            name: 'jack',
+          },
+        ],
+        total: 10,
+      };
+    },
+    defaultFormValues: {
+      username: 'lily',
+    },
+  };
+  const { result } = renderHook(() => useForm(config));
+  const { formProps, form } = result.current;
+  expect(typeof formProps.onFinish).toBe('function');
+  expect(form).toBe(mockForm);
+});
